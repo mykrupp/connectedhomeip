21c1cd3ed4c278e4e002bd3373ebc84a432aecc2
Chirag Bansal <Chirag.Bansal@silabs.com>
2024-05-10 19:42:01 +0000

    Pull request #1830: Increasing the slc generate time to 180 sec
    Merge in WMN_TOOLS/matter from bugfix/increase_generation_time to silabs_slc_1.3
    
    Squashed commit of the following:
    
    commit a430dbfb43d690f522c71aec3d4c47df27b6394d
    Author: chbansal <chirag.bansal@silabs.com>
    Date:   Thu May 9 23:18:53 2024 +0530
    
        increasing the slc generate time to 180 sec

--- slc/build.sh
+++ slc/build.sh
@@ -117,7 +117,7 @@ echo $CONFIG_ARGS
 # Generate project
 
 
-slc generate -d $OUTPUT_DIR $PROJECT_FLAG $SILABS_APP_PATH --with $SILABS_BOARD $CONFIG_ARGS
+slc generate -d $OUTPUT_DIR $PROJECT_FLAG $SILABS_APP_PATH --with $SILABS_BOARD $CONFIG_ARGS --generator-timeout 180
 if [ $? -ne 0 ]; then
     echo "FAILED TO Generate : $SILABS_APP_PATH"
     exit 1
